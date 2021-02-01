0446524abf5d2eb2aff7a812e123e68811b45c38
riley0227 <riley0227@gmail.com>
2021-02-01 08:15:52 -0800

    Change name of screen

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Gay ass homepage'),
+      home: MyHomePage(title: 'Gay ass home'),
     );
   }
 }
